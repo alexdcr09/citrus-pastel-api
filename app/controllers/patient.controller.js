fba6e127907f6a5b0663886c3d114523c46fc959
Jordan Bretton <jbretton@bewizyu.com>
2022-01-19 11:46:06 +0100

    persist user in patient

--- app/controllers/patient.controller.js
+++ app/controllers/patient.controller.js
@@ -3,12 +3,15 @@ const Patient = db.patient;
 
 exports.getAllPatientByUser = (req, res) => {
     Patient.findAll({
-        where: {
-            userId: req.params.userId
-        }
-    }).then(patients => {
+            where: {
+                userId: req.params.userId
+            }
+        },
+        {
+            include: db.user
+        }).then(patients => {
         if (patients.length === 0) {
-            return res.status(404).send({ message: "Aucun patients trouvés."});
+            return res.status(404).send({message: "Aucun patients trouvés."});
         }
 
         return res.status(200).json(patients);
@@ -17,12 +20,15 @@ exports.getAllPatientByUser = (req, res) => {
 
 exports.getPatientById = (req, res) => {
     Patient.findOne({
-        where: {
-            id: req.params.id
-        }
-    }).then(patient => {
+            include: db.user
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }).then(patient => {
         if (!patient) {
-            return res.status(404).send({ message: "Aucun patient trouvé."});
+            return res.status(404).send({message: "Aucun patient trouvé."});
         }
 
         return res.status(200).json(patient);
@@ -37,23 +43,23 @@ exports.createPatient = (req, res) => {
         statut: req.body.statut,
         userId: req.body.userId
     }).then(patient => {
-        res.status(201).send({ message: "Patient créé avec succès!"});
+        res.status(201).send({message: "Patient créé avec succès!"});
     }).catch(err => {
-        res.status(500).send({ message: err.message });
+        res.status(500).send({message: err.message});
     });
 };
 
 exports.updatePatient = (req, res) => {
     Patient.update({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        pathology: req.body.pathology,
-        statut: req.body.statut,
-    }, {returning: true, where: { id: req.params.id }}
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            pathology: req.body.pathology,
+            statut: req.body.statut,
+        }, {returning: true, where: {id: req.params.id}}
     ).then(patient => {
-        res.status(200).send({ message: "Patient modifié avec succès!"});
+        res.status(200).send({message: "Patient modifié avec succès!"});
     })
-    .catch(err => {
-        res.status(500).send({ message: err.message });
-    })
+        .catch(err => {
+            res.status(500).send({message: err.message});
+        })
 };
